a24615d82382238eae236696e20f607e9fd59b54
suvishs <suvishs967@gmail.com>
2023-10-16 12:13:39 +0530

    abcd

--- BMI.py
+++ BMI.py
@@ -1,21 +1,26 @@
+def cal(w,h):
+    h2 = h/100  
+    print("Your HEight In Meters Is ; ", h2)
+    h3=h2*h2
+    bmi=w/h3
+    return bmi
+
+def bmi_value(bmi):
+    if bmi<=18.5:
+        print("You Are Underweight")
+    elif bmi >=18 and bmi <=24.9:
+        print("You Are Normal Weight")
+
+    elif 29.9<=bmi and bmi <=29.9:
+        print("You Are Overweight")
+    else:
+        print("OBESE")    
+
+
 a=input("Enter Your Name")
 print("Welcome to BMI Checking Program ",a)
-
 w=float(input("Enter You Weight(in KG)"))
 h=float(input("Enter your height(in CM)"))
-h2=h/100
-print("Your HEight In Meters Is ; ", h2)
-h3=h2*h2
-bmi=w/h3
-print("Your BMI Is ",bmi)
-
-if bmi<=18.5:
-    print("Underweight")
-elif bmi >=18 and bmi <=24.9:
-    print("Normal Weight")
-
-elif 29.9<=bmi and bmi <=29.9:
-    print("Overweight")
-else:
-    print("obese")    
-
+bmi= cal(w,h)
+print(bmi)
+bmi_value(bmi)
